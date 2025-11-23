bf553e3722f600513e695f64276d71763f05511d
Alex Lute <lute.42@buckeyemail.osu.edu>
2025-11-23 18:06:43 -0500

    world/Direction: fix oopsies

--- +world/Direction.m
+++ +world/Direction.m
@@ -4,7 +4,7 @@ classdef(Enumeration) Direction
     enumeration
         NORTH (-1, 0)
         SOUTH (1, 0)
-        EAST (0, -1)
-        WEST (0, 1)
+        WEST (0, -1)
+        EAST (0, 1)
     end
 end
